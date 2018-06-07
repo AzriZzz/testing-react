0798547e293c6cd0923cde844faaa019aa057101
Muhammad Azri <lenovorosak@gmail.com>
2018-06-07 15:24:18 +0800

    install enzyme

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,12 +3,13 @@ import ReactDom from 'react-dom';
 import App from '../App';
 
 it('it show comment box', () => {
-    const div = document.create('div');
+    const div = document.createElement('div');
 
-    ReactDom.ReactDom(<App />, div);
+    ReactDom.render(<App />, div);
 
     //Looks inside the div
     //and checks to see if the CommentBox is in there
+    expect(div.innerHTML).toContain('Comment Box');
 
     ReactDom.unmountComponentAtNode(div);
 });
